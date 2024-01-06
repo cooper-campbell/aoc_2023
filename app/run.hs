b2afa680466d13138cd29da718b861a8b04638ed
Cooper Campbell <cooper.campbell104@gmail.com>
2024-01-06 14:17:47 -0700

    Add solution to day 1

---
+++ app/run.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE QuasiQuotes, ExtendedDefaultRules #-}
+import System.Environment
+import Text.Printf
+import Data.Maybe
+
+-- Import the days
+import Day1 (run_day)
+
+completed_days = [
+  1
+                 ]
+
+
+
+run_days :: [Int] -> IO ()
+run_days [] = putStrLn "Finished running days"
+run_days (d:rest) = do {
+                       content <- readFile (printf "inputs/day%d.txt" d)
+                       ; putStrLn (show ((solutions !! (d-1)) content))
+                       ; run_days rest
+                       }
+  where
+    solutions = [
+                Day1.run_day
+                ]
+
+main = do
+  args <- getArgs
+  let days = catMaybes [if (read x :: Int) `elem` completed_days then Just (read x :: Int) else Nothing | x <- args]
+  putStrLn ("Ready for days " ++ (show days))
+  
+  run_days days
---
+++ src/Day1.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Day1 (run_day) where
+import qualified Data.Text.Internal.Search as TS
+import qualified Data.Text as T
+-- Needed for sortBy
+import Data.List
+-- For arguments from command line
+import System.Environment
+
+-- Part one only asked for char literals 0-9 to be considered
+map_part_1, map_part_2 :: [(T.Text, Int)]
+map_part_1 = [ ("0", 0), ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9) ]
+-- Part two asks for consideration of spelling numbers 1-9
+map_part_2 = concat [map_part_1, extra_part_2]
+  where extra_part_2 = [ ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9) ]
+
+-- Returns the first and last text-to-number replacements in a string from a map
+get_first_last_num :: [(T.Text, Int)] -> T.Text -> (Int, Int)
+get_first_last_num m s = if (length sorted) == 0 then (0,0) else (snd (head sorted), snd (last sorted))
+  where matches = [(index,val) | (search, val) <- m, index <- TS.indices search s]
+        sorted = sortBy (\x y -> compare (fst x) (fst y)) matches
+
+run_day :: String -> [String]
+run_day input = do
+  let texts = [T.pack x | x <- lines input]
+
+  -- Calculate part 1 and 2
+  let part_1 = sum $ map combine $ map (get_first_last_num map_part_1) texts
+  let part_2 = sum $ map combine $ map (get_first_last_num map_part_2) texts
+
+  -- Print results
+  [("Part 1 : " ++ (show part_1)), ("Part 2 : " ++ (show part_2))]
+    where
+      combine = \(tens, ones) -> 10 * tens + ones
+
+main = do
+  args <- getArgs
+  filecontent <- if (length args) == 0 then readFile filename else readFile (args !! 0)
+  putStrLn (show (run_day filecontent))
+    where filename = "inputs/day1.txt"
